9239060cb424147d9f259a9bc72357d4f8d0d1a9
antoniespinosa <antoniespinosa@me.com>
2013-03-26 09:52:08 +0100

    Martes Inicio

--- wp-content/themes/cjorda/barra_navegacion.php
+++ wp-content/themes/cjorda/barra_navegacion.php
@@ -1,8 +1,8 @@
 <div class="minibarra"></div>
 <div class="barra">
 	<div class="container" id="menu_nav">
-		<div class="btn_barra"><p>HOME</p></div>
-		<div class="btn_barra"><p>CALENDARIO</p></div>
+		<a href="index.php"><div class="btn_barra"><p>HOME</p></div></a>
+		<a href="<?php bloginfo("url");?>/calendario"><div class="btn_barra"><p>CALENDARIO</p></div></a>
 		<div class="btn_barra"><p>GALERIA</p></div>
 		<div class="btn_barra"><p>CIRCUITOS</p></div>
 		<div class="btn_barra"><p>ABOUT ME</p></div>
