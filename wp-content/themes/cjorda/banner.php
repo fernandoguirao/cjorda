f1fbb141be7203c7efd29d6b53785339dce35669
Fernando Guirao <fernando@bueninvento.es>
2013-03-28 11:56:11 +0100

    Cambios Fer últimos

--- wp-content/themes/cjorda/banner.php
+++ wp-content/themes/cjorda/banner.php
@@ -1,5 +1,5 @@
 <div class="banner" >
-	<img src="http://localhost:8888/cjorda/wp-content/uploads/2013/03/slide03.png" alt="">
+	<img src="http://bueninvento.net/proyectos/carmenjorda/wp-content/uploads/2013/03/slide03.png" alt="">
 	<div class="contenedor_titular">
 		<h1 class="titular_slide">
 			<span>
