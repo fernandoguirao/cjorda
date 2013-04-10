e263aa790fe1ac1e14b2d2c2483b68c95881c941
antoniespinosa <antoniespinosa@me.com>
2013-04-10 10:53:59 +0200

    Subida BBDD

--- wp-content/themes/cjorda/copia de index.php
+++ wp-content/themes/cjorda/copia de index.php
@@ -9,8 +9,8 @@
 					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 					$args = array(
 						'category_name'=>'noticias',
-					  'posts_per_page' => 10,
-					  'paged' => $paged
+						'posts_per_page' => 10,
+						'paged' => $paged
   					);
 					query_posts($args); 
 				?>
