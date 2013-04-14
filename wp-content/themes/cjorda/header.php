fb644e699ba71d9cd704604c1676ed6baa404923
Fernando Guirao <fernando@bueninvento.es>
2013-04-14 19:36:44 +0200

    Last weekend

--- wp-content/themes/cjorda/header.php
+++ wp-content/themes/cjorda/header.php
@@ -18,7 +18,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+		<!-- <meta name="viewport" content="width=device-width"> -->
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 		
 		<?php estilos();?>
 		<?php javascripts();?>
